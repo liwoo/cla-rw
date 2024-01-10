a87c7b875d39bbadc7cd44e7806fc3bc9f17c2b8
Jeremiah Chienda <jeremiahchienda@gmail.com>
2024-01-10 23:13:36 +0200

    refactoring `Home` Page to new SEO Rules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {SEOProps} from "@/components/SEO";
 import {BlogSection} from "@/components/index/BlogSection";
 import {Container} from "@/components/Container";
 import {Notice, NoticeWithImage} from "@/components/cta/Notice";
@@ -9,34 +8,27 @@ import {Highlights} from "@/components/index/Highlights";
 import {FindCell} from "@/components/index/FindCell";
 import {MainLayout} from "@/components/layouts/MainLayout";
 import {getLatestNotice, getLatestSermon} from "@/sanity/home-page-data";
-import {Notices, Sermon} from "@/sanity/schema";
+import {Sermon} from "@/sanity/schema";
 import {useNextSanityImage} from "next-sanity-image";
-import client from "@/sanity/client";
-import {createClient} from "@sanity/client";
 import browserClient from "@/sanity/browser-client";
-
-
-const pageDetails: SEOProps = {
-    title: "Welcome to Christian Life Assembly",
-    description: "CLA is a cell based Church that believes in the Bible and the power to change lives through a living relationship with Jesus Christ. Come join us!",
-    image: "https://i.ytimg.com/vi/VKcrBGDRyVc/maxresdefault.jpg",
-    url: "https://clarwanda.org/",
-    keywords: ["Christian Life Assembly", "CLA", "Rwanda", "Church", "Cell Based Church", "Christian", "Jesus", "Christ", "Bible", "Bible Study", "Bible Study Group", "Bible Study Cell"],
-}
-
+import {defaultPageDetails} from "@/utils/default-seo";
 interface ServerProps {
     notice: NoticeWithImage,
     sermon: Sermon
 }
 
-export default function Home({notice, sermon}: ServerProps) {
+export default function Home({notice, sermon}: Readonly<ServerProps>) {
     const welcomeTitle = "Welcome to Christian Life Assembly";
     const welcomeDescription = "CLA is a cell based Church that believes in the Bible and the power to change lives through a living relationship with Jesus Christ. Come join us!";
     return (
-        <MainLayout seo={pageDetails}>
-            <Container className={"mt-8 md:mt-16"}>
+        <MainLayout seo={defaultPageDetails}>
+            <Container className={"mt-8"}>
                 <Notice notice={notice} />
-                <MediaHeroAction sermon={sermon} imageProps={useNextSanityImage(browserClient, sermon.mainImage)} />
+                {
+                    sermon?.mainImage && (
+                        <MediaHeroAction sermon={sermon} imageProps={useNextSanityImage(browserClient, sermon.mainImage)} />
+                    )
+                }
                 <div className="my-4 lg:my-20">
                     <PageTitle title={welcomeTitle} description={welcomeDescription}/>
                 </div>
