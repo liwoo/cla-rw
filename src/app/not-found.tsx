bf19937f4106994593a01c10bac77a5ccee6656b
Jeremiah Chienda <jeremiahchienda@gmail.com>
2024-01-10 23:15:11 +0200

    added new `App Router` components

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import ClientImage from "@/components/ClientImage";
+
+export default function Soon() {
+    return (
+        <main className="relative isolate min-h-full">
+            <ClientImage />
+            <div
+                className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8 text-primary-light text-shadow bg-primary-dark/70">
+                <p className="text-base font-semibold leading-8">We are</p>
+                <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">Coming Soon</h1>
+                <p className="mt-4 text-base sm:mt-6">Please bear with us as this site is still under
+                    construction</p>
+                <div className="mt-10 flex justify-center">
+                    <Link href="/" className="text-sm font-semibold leading-7">
+                        <span aria-hidden="true">&larr;</span> Back to home
+                    </Link>
+                </div>
+            </div>
+        </main>
+    )
+}
---
+++ src/components/cta/ServiceTimes.tsx
@@ -0,0 +1,69 @@
+import { LifebuoyIcon, NewspaperIcon, PhoneIcon } from '@heroicons/react/20/solid'
+import {H2} from "@/components/typography/H2";
+import {H1} from "@/components/typography/H1";
+
+const cards = [
+    {
+        name: 'Adults',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod possimus sit modi rerum exercitationem quaerat atque tenetur ullam.',
+        icon: PhoneIcon,
+    },
+    {
+        name: 'Technical Support',
+        description: 'Quod possimus sit modi rerum exercitationem quaerat atque tenetur ullam.',
+        icon: LifebuoyIcon,
+    },
+    {
+        name: 'Media Inquiries',
+        description: 'Ratione et porro eligendi est sed ratione rerum itaque. Placeat accusantium impedit eum odit.',
+        icon: NewspaperIcon,
+    },
+]
+
+export const ServiceTimes = () => {
+    return (
+        <div className="relative isolate overflow-hidden bg-gray-600 lg:py-16 sm:py-32">
+            <img
+                src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+                alt=""
+                className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+            />
+            <div className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl">
+                <div
+                    className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-50"
+                    style={{
+                        clipPath:
+                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                    }}
+                />
+            </div>
+            <div className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu">
+                <div
+                    className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-50"
+                    style={{
+                        clipPath:
+                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                    }}
+                />
+            </div>
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-2xl lg:mx-0 mb-40 mt-0">
+                    <H2>Service Times</H2>
+                </div>
+                <div className="mx-auto grid max-w-2xl grid-cols-1 gap-6 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8">
+                    {cards.map((card) => (
+                        <div key={card.name} className="flex gap-x-4 rounded-xl p-6">
+                            <div className="text-base leading-7">
+                                <span className={"flex gap-x-4 items-center"}>
+                                    <H1>{card.name}</H1>
+                                    <card.icon className="h-8 w-8 flex-none text-indigo-400" aria-hidden="true" />
+                                </span>
+                                <p className="mt-2 text-gray-300">{card.description}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
