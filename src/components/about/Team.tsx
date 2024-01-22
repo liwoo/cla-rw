acc148091891e867a41fa9b586caebd96b4dc60d
caspater <mkapsata@gmail.com>
2024-01-22 11:46:48 +0200

    fix about page

--- src/components/about/Team.tsx
+++ src/components/about/Team.tsx
@@ -8,7 +8,7 @@ import ClientImage from '@/components/ClientImage'
 
 const Team = () => {
   const categories = [{ name: 'All Staff' }, { name: 'Pastoral Staff' }]
-  const [activeCategory,setActiveCategory]=useState(categories[0]??null)
+  const [activeCategory, setActiveCategory] = useState(categories[0] ?? null)
   const members = Array.from(Array(12).keys()).map((i) => {
     return {
       name: 'Staff Name',
@@ -26,9 +26,11 @@ const Team = () => {
           active={activeCategory}
           color={'text-black'}
           activeBorderColor={'border-primary'}
-          onItemClick={(item)=>{setActiveCategory(item)}}
+          onItemClick={(item) => {
+            setActiveCategory(item)
+          }}
         />
-        <div className="mt-6 grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 3xl:grid-cols-6 gap-16">
+        <div className="xs:grid-cols-2 3xl:grid-cols-6 mt-6 grid grid-cols-1 gap-16 sm:grid-cols-3 lg:grid-cols-4">
           {members.map((item) => {
             return <Member key={item.name} {...item} />
           })}
@@ -49,11 +51,7 @@ const Member = ({ name, office, imgSrc }: MemberProps) => {
   return (
     <div>
       <div className="relative my-4 w-full overflow-hidden rounded-md pt-[100%]">
-        <ClientImage
-          fill
-          src={imgSrc}
-          alt={name}
-        />
+        <ClientImage fill src={imgSrc} alt={name} />
       </div>
       <div className="font-semibold">{name}</div>
       <div className="font-extralight">{office}</div>
