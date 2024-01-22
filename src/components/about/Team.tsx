33789b283064ec7e13068bb64b59786fa7e92280
caspater <mkapsata@gmail.com>
2024-01-22 20:48:00 +0200

    merge develop

--- src/components/about/Team.tsx
+++ src/components/about/Team.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+"use client"
+import  { useState } from 'react'
 import { PageTitle } from '@/components/PageTitle'
 import SubMenu from '@/components/SubMenu'
 import { Container } from '@/components/Container'
