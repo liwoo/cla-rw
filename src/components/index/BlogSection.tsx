f5438c6c57a309126e10f3681e4c60467d45002f
Marumbo <marumbok@gmail.com>
2024-01-25 01:14:02 +0200

    added workspace typescript check

--- src/components/index/BlogSection.tsx
+++ src/components/index/BlogSection.tsx
@@ -4,14 +4,13 @@ import { H2 } from '@/components/typography/H2'
 import { Subheading } from '@/components/typography/Subheading'
 import { CalendarIcon } from '@heroicons/react/20/solid'
 import { Post } from '@/sanity/schema'
-import {useNextSanityImage, UseNextSanityImageProps} from "next-sanity-image";
-import browserClient from "@/sanity/browser-client";
-import Link from 'next/link';
+import { useNextSanityImage, UseNextSanityImageProps } from 'next-sanity-image'
+import browserClient from '@/sanity/browser-client'
+import Link from 'next/link'
 import { getHumanReadableDate } from '@/utils/helpers'
 
-
-
-export const BlogSection = ({posts}: {posts:Post[]}) => {
+export const BlogSection = ({ posts }: { posts: Post[] }) => {
+  console.log('post 1 author')
   return (
     <div className="bg-white py-12 sm:py-24">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -19,7 +18,7 @@ export const BlogSection = ({posts}: {posts:Post[]}) => {
           <PageTitle title="Latest Posts & Devotionals" />
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post,index) => (
+          {posts.map((post, index) => (
             <article
               key={index}
               className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-60 sm:pt-48 lg:pt-60"
@@ -29,8 +28,9 @@ export const BlogSection = ({posts}: {posts:Post[]}) => {
                 height={500}
                 {...useNextSanityImage(browserClient, post.mainImage)}
                 alt=""
-                className="absolute inset-0 -z-10 aspect-video h-full w-full object-cover"
+                className="aspect-video absolute inset-0 -z-10 h-full w-full object-cover"
               />
+
               <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
               <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
 
@@ -48,7 +48,7 @@ export const BlogSection = ({posts}: {posts:Post[]}) => {
                   aria-hidden="true"
                 />
                 <time dateTime={post.publishedAt} className="mr-8">
-                {getHumanReadableDate(post._updatedAt)}
+                  {getHumanReadableDate(post._updatedAt)}
                 </time>
               </Subheading>
             </article>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,23 @@
-import React from "react";
-import {SEO, SEOProps} from "@/components/SEO";
-import {BlogSection} from "@/components/index/BlogSection";
-import {Container} from "@/components/Container";
-import {Notice, NoticeWithImage} from "@/components/cta/Notice";
-import {MediaHeroAction} from "@/components/index/MediaHeroAction";
-import {PageTitle} from "@/components/PageTitle";
-import {Highlights} from "@/components/index/Highlights";
-import {FindCell} from "@/components/index/FindCell";
-import {getCallToActions, getCells, getLatestDevotionals, getLatestNotice, getLatestSermon} from "@/sanity/home-page-data";
-import { MainLayout } from "@/components/layouts/MainLayout";
-import browserClient from "@/sanity/browser-client";
-import { CallToAction, Post, Sermon, ZoneSection } from "@/sanity/schema";
-import { useNextSanityImage } from "next-sanity-image";
-
-
+import React from 'react'
+import { SEO, SEOProps } from '@/components/SEO'
+import { BlogSection } from '@/components/index/BlogSection'
+import { Container } from '@/components/Container'
+import { Notice, NoticeWithImage } from '@/components/cta/Notice'
+import { MediaHeroAction } from '@/components/index/MediaHeroAction'
+import { PageTitle } from '@/components/PageTitle'
+import { Highlights } from '@/components/index/Highlights'
+import { FindCell } from '@/components/index/FindCell'
+import {
+  getCallToActions,
+  getCells,
+  getLatestDevotionals,
+  getLatestNotice,
+  getLatestSermon,
+} from '@/sanity/home-page-data'
+import { MainLayout } from '@/components/layouts/MainLayout'
+import browserClient from '@/sanity/browser-client'
+import { CallToAction, Post, Sermon, ZoneSection } from '@/sanity/schema'
+import { useNextSanityImage } from 'next-sanity-image'
 
 const pageDetails: SEOProps = {
   title: 'Welcome to Christian Life Assembly',
@@ -38,52 +42,63 @@ const pageDetails: SEOProps = {
 }
 
 interface ServerProps {
-    notice: NoticeWithImage,
-    sermon: Sermon,
-    devotionals:Post[],
-    callToActions : CallToAction[]
-    cells: ZoneSection[]
+  notice: NoticeWithImage
+  sermon: Sermon
+  devotionals: Post[]
+  callToActions: CallToAction[]
+  cells: ZoneSection[]
 }
 
-export default function Home({notice, sermon,devotionals,callToActions,cells}: ServerProps) {
-  
-    const welcomeCta = callToActions.find(highlight => highlight.order ===1);
-    const highlights = callToActions.filter(cta => cta.order >= 2 && cta.order <=6)
-    const cellcta = callToActions.find(highlight => highlight.order ===7);
-    
+export default function Home({
+  notice,
+  sermon,
+  devotionals,
+  callToActions,
+  cells,
+}: ServerProps) {
+  const welcomeCta = callToActions.find((highlight) => highlight.order === 1)
+  const highlights = callToActions.filter(
+    (cta) => cta.order >= 2 && cta.order <= 6
+  )
+  const cellcta = callToActions.find((highlight) => highlight.order === 7)
 
-    return (
-        <MainLayout seo={pageDetails}>
-            <Container className={"mt-8 md:mt-16"}>
-                <Notice notice={notice} />
-                <MediaHeroAction sermon={sermon} imageProps={useNextSanityImage(browserClient, sermon.mainImage)} />
-                <div className="my-4 lg:my-20">
-                    <PageTitle title={welcomeCta.title} description={welcomeCta.description}/>
-                </div>
-                <Highlights highlights= {highlights}/>
-            </Container>
-            <FindCell cell = {cellcta} cells = {cells}/>
-            <BlogSection posts={devotionals} />
-        </MainLayout>
-    )
+  return (
+    <MainLayout seo={pageDetails}>
+      <Container className={'mt-8 md:mt-16'}>
+        <Notice notice={notice} />
+        <MediaHeroAction
+          sermon={sermon}
+          imageProps={useNextSanityImage(browserClient, sermon.mainImage)}
+        />
+        <div className="my-4 lg:my-20">
+          <PageTitle
+            title={welcomeCta.title}
+            description={welcomeCta.description}
+          />
+        </div>
+        <Highlights highlights={highlights} />
+      </Container>
+      <FindCell cell={cellcta} cells={cells} />
+      <BlogSection posts={devotionals} />
+    </MainLayout>
+  )
 }
 
 export async function getStaticProps() {
-    // do whatever you want to do to get your props
-    const notice = await getLatestNotice();
-    const devotionals = await getLatestDevotionals();
-    const sermon = await getLatestSermon();
-    const callToActions = await getCallToActions();
-    const cells = await getCells();
+  // do whatever you want to do to get your props
+  const notice = await getLatestNotice()
+  const devotionals = await getLatestDevotionals()
+  const sermon = await getLatestSermon()
+  const callToActions = await getCallToActions()
+  const cells = await getCells()
 
-    
-    return {
-        props: {
-           notice,
-            sermon,
-            devotionals,
-            callToActions,
-            cells
-        },
-    }
+  return {
+    props: {
+      notice,
+      sermon,
+      devotionals,
+      callToActions,
+      cells,
+    },
+  }
 }
