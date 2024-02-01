c70f4e6b7b14c713d674c4e2898935c1a4e9fe46
caspater <mkapsata@gmail.com>
2024-02-01 15:46:45 +0200

    refactor events

--- src/components/events/EventsList/EventsList.tsx
+++ src/components/events/EventsList/EventsList.tsx
@@ -4,23 +4,22 @@ import EventCard from './EventCard'
 import { Container } from '@/components/Container'
 import { LargeButton } from '@/components/LargeButton'
 import SubMenu from '@/components/SubMenu'
+import { EventItem, MenuItem } from '@/utils/types'
+import { useState } from 'react'
+import { eventsFilters } from '@/data/ui'
 
 const EventsList = () => {
-  const events = Array.from(Array(8).keys()).map(() => ({
-    title: 'Title',
+  const events:EventItem[]= Array.from(Array(8).keys()).map(() => ({
+    title: 'Event Title',
+    type:"Event Type",
+    date:'10-10-2024',
+    startTime:"18:00",
+    endTime:"18:30",
+    description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia culpa velit fugiat? Quas fuga, ad hic, tempora temporibus voluptates, ut quo porro accusamus sint pariatur rerum harum commodi omnis veniam.",
+    coverImg:"https://res.cloudinary.com/c99/image/upload/v1705326472/Placeholders/Screenshot_2024-01-15_at_15.47.16.png"
   }))
 
-  const filters = [
-    {
-      name: 'All',
-    },
-    {
-      name: 'Upcoming',
-    },
-    {
-      name: 'Past',
-    },
-  ]
+  const [filter,setFilter]=useState(eventsFilters[0] as MenuItem )
 
   return (
     <section className="bg-surface py-16">
@@ -29,16 +28,16 @@ const EventsList = () => {
         <SubMenu
           color="text-black"
           activeBorderColor="border-primary"
-          items={filters}
-          active={filters[0]}
-          onItemClick={() => {}}
+          items={eventsFilters}
+          active={filter}
+          onItemClick={(filter:MenuItem) => setFilter(filter)}
         />
       </div>
 
       <Container>
-        <div className="grid grid-cols-2 gap-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
           {events.map((event) => (
-            <EventCard key={event.title} />
+            <EventCard key={event.title} event={event} />
           ))}
         </div>
         <div className="my-16 text-center">
--- src/components/events/Hero/Hero.tsx
+++ src/components/events/Hero/Hero.tsx
@@ -1,17 +1,14 @@
-'use client'
-
+import ClientImage from '@/components/ClientImage'
 import { Container } from '@/components/Container'
 import { LargeButton } from '@/components/LargeButton'
 import { ArrowRightIcon, FolderOpenIcon } from '@heroicons/react/20/solid'
-import Image from 'next/image'
 
 const Hero = () => {
   return (
     <div className="relative min-h-[500px]">
-      <Image
+      <ClientImage
         fill
         quality={50}
-        placeholder={'blur'}
         blurDataURL={
           'https://res.cloudinary.com/tiyeni/image/upload/c_scale,h_678,q_auto:low/v1679808591/2X0A4983.jpg'
         }
@@ -21,54 +18,61 @@ const Hero = () => {
       />
       <Container>
         <div className="relative py-40 text-white">
-          <div className='font-bold text-2xl my-2'>Upcoming Event:</div>
-          <div className='font-bold text-4xl my-2'>Event Name</div>
-          <div className='my-2'>
-            <FolderOpenIcon className="h-6" />
+          <div className="my-2 text-lg font-bold md:text-2xl">
+            Upcoming Event:
+          </div>
+          <div className="my-2 text-2xl font-bold md:text-4xl">Event Name</div>
+          <div className="my-2 flex items-center">
+            <span className="mr-4 text-lg font-bold text-yellow-300 md:text-2xl">
+              Adult
+            </span>
+            <FolderOpenIcon className="h-8 text-yellow-300" />
           </div>
         </div>
       </Container>
       <div className="relative  w-full bg-yellow-300">
-        <div className="absolute left-0 bottom-0 z-0 w-full bg-yellow-300"></div>
+        <div className="absolute bottom-0 left-0 z-0 w-full bg-yellow-300"></div>
         <Container>
-          <div className="flex justify-between items-center ">
-            <div className="relative flex-1">
-              <div className="flex justify-between items-center">
-                <div>
-                  <div className='font-bold text-3xl'>Title</div>
-                  <div className='font-semibold my-2'>Content</div>
-                </div>
-                <div className="border border-black pt-[10%]"></div>
-                <div>
-                  <div className='font-bold text-3xl'>Title</div>
-                  <div className='font-semibold my-2'>Content</div>
-                </div>
-                <div className="border border-black pt-[10%]"></div>
-                <div>
-                  <div className='font-bold text-3xl'>Title</div>
-                  <div className='font-semibold my-2'>Content</div>
-                </div>
+          <div className="block items-center justify-evenly lg:justify-between py-6 md:py-0 md:flex">
+            <div className="relative p-6 lg:p-0 flex-1">
+              <div className="block items-center justify-between md:flex">
+                <DetailItem title={'date'} value={'2 Apr'} />
+                <div className="hidden border-[0.5px] border-black pt-[20%] md:block"></div>
+                <DetailItem title={'venue'} value={'Namiwawa'} />
+                <div className="hidden border-[0.5px] border-black pt-[20%] md:block"></div>
+                <DetailItem title={'time'} value={'12:00'} />
               </div>
-              <div className="flex justify-end mt-8 items-end"><LargeButton>Register</LargeButton></div>
+              <div className="mt-8 flex items-end justify-start md:justify-end">
+                <LargeButton>Register</LargeButton>
+              </div>
             </div>
-            <div className="relative mx-12 w-[30%] pt-[25%]">
-              <Image
+            <div className="relative mx-12 hidden w-[30%] pt-[25%] lg:block">
+              <ClientImage
                 fill
                 quality={50}
-                placeholder={'blur'}
                 blurDataURL={
                   'https://res.cloudinary.com/tiyeni/image/upload/c_scale,h_678,q_auto:low/v1679808591/2X0A4983.jpg'
                 }
                 src="https://res.cloudinary.com/c99/image/upload/v1705326472/Placeholders/Screenshot_2024-01-15_at_15.47.16.png"
                 alt="Coming Soon"
-                className="absolute !h-[110%] w-full object-cover !-top-[10%]"
+                className="absolute !-top-[10%] !h-[110%] w-full object-cover"
               />
             </div>
 
-            <div className="relative w-[25%]">
-              <div >
-                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores accusamus enim totam distinctio, quibusdam quasi laboriosam provident, ducimus modi vero similique ipsum id possimus voluptatem non debitis corporis, excepturi quam.</div>
-                <div className="flex items-center my-4"><span className='font-bold mr-2'>Read More</span><ArrowRightIcon className='h-6'/></div>
+            <div className="relative py-6 w-full lg:py-0 md:w-[35%] lg:w-[25%]">
+              <div>
+                <div>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Dolores accusamus enim totam distinctio, quibusdam quasi
+                  laboriosam provident, ducimus modi vero similique ipsum id
+                  possimus voluptatem non debitis corporis, excepturi quam.
+                </div>
+                <div className="my-4 flex items-center">
+                  <span className="mr-2 cursor-pointer font-bold">
+                    Read More
+                  </span>
+                  <ArrowRightIcon className="h-6" />
+                </div>
               </div>
             </div>
           </div>
@@ -79,3 +83,16 @@ const Hero = () => {
 }
 
 export default Hero
+
+interface DetailItemProps {
+  title: string
+  value: string
+}
+const DetailItem = ({ title, value }: DetailItemProps) => {
+  return (
+    <div className="my-4">
+      <div className="text-2xl font-bold lg:text-3xl">{value}</div>
+      <div className="my-2 font-semibold uppercase">{title}</div>
+    </div>
+  )
+}
