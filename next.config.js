235ba36164a3c5e4d4caa495df8f32565996e9cd
Marumbo <marumbok@gmail.com>
2024-02-09 09:40:44 +0200

    added footer data from config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,19 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com', 'images.unsplash.com', 'cdn.sanity.io'],
   },
+  publicRuntimeConfig: {
+    footerChurchDescription: `Christian Life Assembly is a multi-national, 
+      multi-cultural Church that believes in the changing power of the Word of God 
+      and that seeks to see transformation of our communities through the love of Jesus Christ.`,
+    footerChurchAddress: '2 KG 270 St, Kigali',
+    footerChurchEmail: 'info@clarwanda.com',
+    footerChurchNumber: '0788 302 013',
+    footerChurchYoutube:
+      'https://www.youtube.com/channel/UClwCYTUDAa2PCLPxQ46BfkA',
+    footerChurchInstagram: 'https://www.instagram.com/clarwanda/?hl=en',
+    footerChurchFacebook: 'https://www.facebook.com/clarwanda/',
+    footerChurchTwitter: 'https://twitter.com/clarwanda',
+  },
 }
 
 module.exports = nextConfig
