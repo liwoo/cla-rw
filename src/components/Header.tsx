27621c992748270b6f0af9ef094789692e8f0276
Jeremiah Chienda <jeremiahchienda@gmail.com>
2024-03-03 05:14:58 +0200

    soft links on mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -223,11 +223,11 @@ export const Header: FC<{ company: string }> = ({ company }) => {
                                     {men.subMenu?.map((item) => (
                                       <Disclosure.Button
                                         key={item.name}
-                                        as="a"
-                                        href={item.href}
                                         className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                       >
-                                        {item.name}
+                                        <Link href={item.href}>
+                                          {item.name}
+                                        </Link>
                                       </Disclosure.Button>
                                     ))}
                                   </Disclosure.Panel>
